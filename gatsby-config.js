27570cf980cd580a3e6d7c2431d60663f4530a21
influential <pay10olson@gmail.com>
2022-12-03 14:22:51 -0800

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@ module.exports = {
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "images",
-      "path": "./src/images/"
+      "name": "assets",
+      "path": "./src/assets/"
     },
-    __key: "images"
+    __key: "assets"
   }]
 };
